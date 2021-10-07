c3123b6e37c066b162c477352c407901ac2c775e
saurabh <saurabh@litmusworld.com>
2021-10-07 17:04:20 +0530

    register/login/logit/getuser functionality added

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,6 +67,7 @@ exports.logout = asyncHandler(async (req, res, next) => {
 //@route GET /blogbook/v1/auth/me
 //@access private
 exports.getMe = asyncHandler(async (req, res, next) => {
+    
     const user = await User.findById(req.user.id);
 
     res.status(200).json({
